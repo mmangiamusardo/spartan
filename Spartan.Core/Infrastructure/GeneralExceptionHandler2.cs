e0d02171301ad05407b91e7537d4440f2e2940a0
Marco Mangia <m.mangiamusardo@gmail.com>
2016-02-01 17:37:35 +0100

    General Exception Handler v2

---
+++ Spartan.Core/Infrastructure/GeneralExceptionHandler2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.ExceptionHandling;
+
+namespace Spartan.Core
+{
+    public class GeneralExceptionHandler : ExceptionHandler
+    {
+        public override void Handle(ExceptionHandlerContext context)
+        {
+            context.Result = new GeneralErrorResult
+            {
+                Request = context.ExceptionContext.Request,
+                Content = "An error has occurred on the server. Please contact support!"
+            };
+        }
+    }
+}
