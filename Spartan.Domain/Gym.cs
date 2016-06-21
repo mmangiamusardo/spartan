31515a0692ae5eecae17163e0abc0a6c08903341
Marco Mangia <m.mangiamusardo@gmail.com>
2016-06-22 00:28:45 +0200

    Added base class

--- Spartan.Domain/Gym.cs
+++ Spartan.Domain/Gym.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Spartan.Domain
 {
-    public class Gym
+    public class Gym : SpartanBase
     {
         public int Id { get; set; }
         public string Name { get; set; }
